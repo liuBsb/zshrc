72ec1a100850add8195db28cca4a4b69d0b6d651
Sebastian <sebastian.rs.ba@gmail.com>
2023-12-27 14:52:05 -0300

    third-party: add plugin powerlevel10k

--- third-party/powerlevel10k.zsh
+++ third-party/powerlevel10k.zsh
@@ -1,1 +1,10 @@
-powerlevel10k/powerlevel10k.zsh
+# enable instant prompt
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+# Show prompt segment "kubecontext" only when the command you are typing invokes one of these tools.
+typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='kubectl|helm|kubens'
+
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+source $ZSH/third-party/powerlevle10k/powerlevle10k.zsh
+
